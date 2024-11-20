2af0bfcfac7c5dd627c307e6ec987862d93318f9
Antonio Rodriguez <arodriguez-oc@ryanrts.com>
2024-11-20 08:50:48 -0300

    fix(release): fixing Grid component not exported

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./index.css";
 export { Button } from "./Components/Button/Button";
 export type { ButtonProps } from "./Components/Button/Button";
-export { Stack } from "./Layout/Stack/Stack";
 export { Text } from "./Components/Text/Text";
 export { InfoCard } from "./Components/NextTutorial/InfoCard";
+export { Stack } from "./Layout/Stack/Stack";
+export { Grid } from "./Layout/Grid/Grid";
