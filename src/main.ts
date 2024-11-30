c7335dc8404823019da22e82f447e147b03d53c9
Antonio Rodriguez <arodriguez-oc@ryanrts.com>
2024-11-30 17:07:17 -0300

    fix(Exporting the new component): Exporting the ChartRevenue Component and Revenue Type

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import "./index.css";
 export { Button } from "./Components/Button/Button";
-export type { ButtonProps } from "./Components/Button/Button";
 export { Text } from "./Components/Text/Text";
 export { InfoCard } from "./Components/NextTutorial/InfoCard";
 export { Stack } from "./Layout/Stack/Stack";
 export { Grid } from "./Layout/Grid/Grid";
+export { RevenueChart } from "./Components/NextTutorial/RevenueChart";
+
+// Export types if needed
+export type { ButtonProps } from "./Components/Button/Button";
+export type { Revenue } from "./utils/utils";
