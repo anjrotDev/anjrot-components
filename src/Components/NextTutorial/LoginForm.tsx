55ca627ce6d6012807aa46c4567153ce9c2a1f89
Antonio Rodriguez <arodriguez-oc@ryanrts.com>
2025-02-07 18:05:01 -0300

    fix(disable button): adding aria-disable to button

--- src/Components/NextTutorial/LoginForm.tsx
+++ src/Components/NextTutorial/LoginForm.tsx
@@ -4,9 +4,10 @@ import { Button } from "../Button/Button";
 
 interface LoginFormProps extends FormHTMLAttributes<HTMLFormElement> {
   error?: string;
+  disabled?: boolean;
 }
 
-export const LoginForm: FC<LoginFormProps> = ({ error, className, action, ...props }) => {
+export const LoginForm: FC<LoginFormProps> = ({ error, className, action, disabled, ...props }) => {
   return (
     <form className="space-y-3" action={action} {...props}>
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -49,6 +50,7 @@ export const LoginForm: FC<LoginFormProps> = ({ error, className, action, ...pro
         <Button
           type="submit"
           className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50 mt-4 w-full"
+          aria-disabled={disabled}
         >
           Log in
         </Button>
